5cfbafda18e6eb2bc533fa2d67b1d887941aa605
PavelBakanov <maxconghunter@mail.ru>
2024-11-13 16:35:44 +0300

    minor changes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -21,6 +22,6 @@ public class TestBase {
 
     @AfterEach
     void tearDown() {
-        closeWebDriver();
+        Selenide.closeWebDriver();
     }
 }
